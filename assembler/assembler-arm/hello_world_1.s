957251368cc545e40092043ff980dea5334b5a6f
Eoin Kelly <eoin@eoinkelly.info>
2016-08-21 17:41:31 +1200

    Clean up arm assembler code

--- assembler/assembler-arm/hello_world_1.s
+++ assembler/assembler-arm/hello_world_1.s
@@ -11,10 +11,24 @@ str: 	.asciz	"Hello world!\n"	@ this is inline comment
 	.globl main
 
 main:
-	@@ stmfd 	sp!, [lr]
-	ldr	r0, =str
-	bl	printf
-	mov	r0, #0
-	@@ ldmfd	sp!, [lr]
-	mov 	pc, lr
+	@ function setup
+
+	@@ This does the following:
+	@@
+	@@	store multiple, full descending (decrement before), modify sp
+        @@
+	@@ 1. drecement the value of sp
+	@@ 1. store the contents of the lr register at the address pointed to by sp
+	@@
+	stmfd 	sp!, {lr}	@ push return address onto stack
+
+	@ print the message
+	@@ QUESTION: why ldr here an mov later?
+	ldr	r0, =str	@ load address of str into r0
+	bl	printf		@ call printf
+
+	@ function teardown
+	mov	r0, #0		@ put program return code in r0
+	ldmfd	sp!, {lr}	@ pop return address from stack
+	mov 	pc, lr		@ return and exit program
 
