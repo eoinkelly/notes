e17add451347b901d2346dbba57e04ff52eda13b
Eoin Kelly <eoin@rabidtech.co.nz>
2019-02-13 07:05:16 +1300

    Remove ecto_play because it had little learning value

--- elixir/ecto_play/lib/ecto_play.ex
+++
@@ -1,6 +0,0 @@
-defmodule EctoPlay do
-end
-
-defmodule EctoPlay.Repo do
-  use Ecto.Repo, otp_app: :ecto_play
-end
--- elixir/ecto_play/test/ecto_play_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule EctoPlayTest do
-  use ExUnit.Case
-  doctest EctoPlay
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
