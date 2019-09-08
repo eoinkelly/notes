2aae807005c18c6502c08b97e536b88ad55c7e6b
Eoin Kelly <eoin@rabidtech.co.nz>
2019-09-08 20:53:46 +1200

    Tweak JOIN in ruby script

--- databases/postgresql/code/joins_in_ruby.rb
+++ databases/postgresql/code/joins_in_ruby.rb
@@ -1,85 +1,182 @@
+##
 # * Implement SQL joins for Ruby Array of Arrays.
-# * The Ruby code is designed to help me understand the algorithms rather than be idiomatic Ruby
-# * I'm only confident that these algorithms are correct to the extent that the tests inlined below pass. There are probably edge cases I'm not aware of.
-# All SQL tested on Postgres 11.3
+# * I'm only confident that these algorithms are correct to the extent that the
+#   tests inlined below pass. There are probably edge cases I'm not aware of.
+# * All SQL tested on Postgres 11.3
+# * All tables are represented as Array of Array
+# * Aside: You can add arrays in ruby with `+`
+# * I use the following jargon below:
+#     * LHS = left hand side
+#     * RHS = right hand side
+#
 
+##
+# Generate an array of nils of the given length
+#
+# @param length [Integer]
+# @return [Array<nil>]
+#
+def array_of_nils(length:)
+  Array.new(length, nil)
+end
+
+##
+# @param lhs_table [Array]
+# @param rhs_table [Array]
+# @return [Array]
+#
 def cross_join(lhs_table, rhs_table)
   output = []
 
-  # TODO: the rhs and lhs are iterated the opposite way to what I would expect?
+  # Note: CROSS JOIN iterates across the RHS table first. This may be
+  # surprising to you - it was to me :-)
   rhs_table.each do |rhs_row|
     lhs_table.each do |lhs_row|
-        output << lhs_row + rhs_row
+      output << lhs_row + rhs_row
     end
   end
 
   output
 end
 
+##
+# @param lhs_table [Array]
+# @param rhs_table [Array]
+#
+# @yield [lhs_row, rhs_row] a block implementing the join condition by
+#   comparing the two given rows in some way
+#
+# @yieldparam lhs_row [Array]
+# @yieldparam rhs_row [Array]
+# @yieldreturn [Boolean]
+#
+# @return [Array]
+#
 def inner_join(lhs_table, rhs_table, &block)
   output = []
 
   lhs_table.each do |lhs_row|
     rhs_table.each do |rhs_row|
-        if block.call(lhs_row, rhs_row)
-          output << lhs_row + rhs_row
-        else
-          # do nothing
-        end
+      if block.call(lhs_row, rhs_row)
+        output << lhs_row + rhs_row
+      else
+        # do nothing
+      end
     end
   end
 
   output
 end
 
+##
+# @param lhs_table [Array]
+# @param rhs_table [Array]
+#
+# @yield [lhs_row, rhs_row] a block implementing the join condition by
+#   comparing the two given rows in some way
+#
+# @yieldparam lhs_row [Array]
+# @yieldparam rhs_row [Array]
+# @yieldreturn [Boolean]
+#
+# @return [Array]
+#
 def left_join(lhs_table, rhs_table, &block)
   output = []
   unmatched_lhs_rows = []
 
+  # for each row in the LHS table ...
   lhs_table.each do |lhs_row|
+    # ... find all rows in the RHS table which match this row (using the join
+    # condition block to test)
     matching_rhs_rows = rhs_table.select do |rhs_row|
       block.call(lhs_row, rhs_row)
     end
 
-    matching_rhs_rows.each do |rhs_row|
-      output << lhs_row + rhs_row
+    # if we didn't find any RHS rows which match the current LHS row then we
+    # store the current LHS row in our "unmatched" list so we can append those
+    # rows to the output **after** all the matched rows.
+    if matching_rhs_rows.empty?
+      unmatched_lhs_rows << lhs_row
+    else
+      # for each matching row in the RHS table, build a new row by
+      # concatenating the LHS and RHS rows and add it to the output.
+      matching_rhs_rows.each do |rhs_row|
+        output << lhs_row + rhs_row
+      end
     end
-
-    unmatched_lhs_rows << lhs_row if matching_rhs_rows.length == 0
   end
 
-  # only add the unmatched rows after we have added all the matching ones
+  # Add the unmatched rows from the LHS table **after** we have added all the
+  # matching rows.
   unmatched_lhs_rows.each do |lhs_row|
-    output << lhs_row + Array.new(rhs_table.first.length, nil)
+    output << lhs_row + array_of_nils(length: rhs_table.first.length)
   end
 
   output
 end
 
+##
+# @param lhs_table [Array]
+# @param rhs_table [Array]
+#
+# @yield [lhs_row, rhs_row] a block implementing the join condition by
+#   comparing the two given rows in some way
+#
+# @yieldparam lhs_row [Array]
+# @yieldparam rhs_row [Array]
+# @yieldreturn [Boolean]
+#
+# @return [Array]
+#
 def right_join(lhs_table, rhs_table, &block)
   output = []
   unmatched_rhs_rows = []
 
+  # for each row in the RHS table ...
   rhs_table.each do |rhs_row|
+    # ... find all rows in the LHS table which match this row (using the join
+    # condition block to test)
     matching_lhs_rows = lhs_table.select do |lhs_row|
       block.call(lhs_row, rhs_row)
     end
 
-    matching_lhs_rows.each do |lhs_row|
-      output << lhs_row + rhs_row
+    # if we didn't find any LHS rows which match the current RHS row then we
+    # store the current RHS row in our "unmatched" list so we can append those
+    # rows to the output **after** all the matched rows.
+    if matching_lhs_rows.empty?
+      unmatched_rhs_rows << rhs_row
+    else
+      # for each matching row in the LHS table, build a new row by
+      # concatenating the LHS and RHS rows and add it to the output.
+      matching_lhs_rows.each do |lhs_row|
+        output << lhs_row + rhs_row
+      end
     end
-
-    unmatched_rhs_rows << rhs_row if matching_lhs_rows.length == 0
   end
 
-  # only add the unmatched rows after we have added all the matching ones
+  # Add the unmatched rows from the RHS table **after** we have added all the
+  # matching rows.
   unmatched_rhs_rows.each do |rhs_row|
-    output << Array.new(lhs_table.first.length, nil) + rhs_row
+    output << array_of_nils(length: lhs_table.first.length) + rhs_row
   end
 
   output
 end
 
+##
+# @param lhs_table [Array]
+# @param rhs_table [Array]
+#
+# @yield [lhs_row, rhs_row] a block implementing the join condition by
+#   comparing the two given rows in some way
+#
+# @yieldparam lhs_row [Array]
+# @yieldparam rhs_row [Array]
+# @yieldreturn [Boolean]
+#
+# @return [Array]
+#
 def full_join(lhs_table, rhs_table, &block)
   # create a UNION of a LEFT JOIN and a RIGHT JOIN
   output = left_join(lhs_table, rhs_table, &block) + right_join(lhs_table, rhs_table, &block)
@@ -88,26 +185,32 @@ def full_join(lhs_table, rhs_table, &block)
   output.uniq
 end
 
-# create table users (id bigint primary key not null, name text, org_id bigint);
-# create table organisations (id bigint primary key not null, name text);
-# insert into users (id, name, org_id) values (1, 'joe', 1), (2, 'mary', 1), (3, 'brian', 3), (4, 'mike', null);
-# insert into organisations (id, name) values (1, 'Foo Inc'), (2, 'Bar Inc'), (3, 'Baz Inc');
+##
+# Create the test data as both SQL and Ruby arrays
 
+# SQL
+#
+#   create table users (id bigint primary key not null, name text, org_id bigint);
+#   create table organisations (id bigint primary key not null, name text);
+#   insert into users (id, name, org_id) values (1, 'joe', 1), (2, 'mary', 1), (3, 'brian', 3), (4, 'mike', null);
+#   insert into organisations (id, name) values (1, 'Foo Inc'), (2, 'Bar Inc'), (3, 'Baz Inc');
+
+# Ruby
+#
 users = [
   # id, name, org_id
-  [ 1, "joe", 1 ],
-  [ 2, "mary", 1 ],
-  [ 3, "brian", 3 ],
-  [ 4, "mike", nil ],
+  [1, "joe", 1],
+  [2, "mary", 1],
+  [3, "brian", 3],
+  [4, "mike", nil]
 ]
 
 organisations = [
   # id, name
-  [ 1, "Foo Inc." ],
-  [ 2, "Bar Inc." ],
-  [ 3, "Baz Inc." ],
+  [1, "Foo Inc."],
+  [2, "Bar Inc."],
+  [3, "Baz Inc."]
 ]
-
 
 # ########################################################################### #
 
@@ -118,9 +221,7 @@ cross_join_results = cross_join(users, organisations)
 pp cross_join_results
 puts "Found #{cross_join_results.length} rows."
 
-# SQL output:
-#
-# postgres@eoin=#  select * from users CROSS JOIN organisations;
+# postgres@eoin=#  SELECT * FROM users CROSS JOIN organisations;
 #  id │ name  │ org_id │ id │  name
 # ════╪═══════╪════════╪════╪═════════
 #   1 │ joe   │      1 │  1 │ Foo Inc
@@ -156,9 +257,7 @@ puts "Found #{cross_join_results.length} rows."
 #  [4, "mike", nil, 3, "Baz Inc."]]
 # Found 12 rows.
 
-
 # ########################################################################### #
-
 puts "*" * 80
 puts "users INNER JOIN organisations"
 puts ""
@@ -170,7 +269,7 @@ end
 pp inner_join_results
 puts "Found #{inner_join_results.length} rows."
 
-# postgres@eoin=#  select * from users AS u INNER JOIN organisations AS o ON u.org_id = o.id;
+# postgres@eoin=#  SELECT * FROM users AS u INNER JOIN organisations AS o ON u.org_id = o.id;
 #  id │ name  │ org_id │ id │  name
 # ════╪═══════╪════════╪════╪═════════
 #   1 │ joe   │      1 │  1 │ Foo Inc
@@ -190,7 +289,6 @@ puts "Found #{inner_join_results.length} rows."
 #  [3, "brian", 3, 3, "Baz Inc."]]
 # Found 3 rows.
 
-
 # ########################################################################### #
 puts "*" * 80
 puts "users LEFT JOIN organisations"
@@ -203,7 +301,7 @@ end
 pp left_join_results
 puts "Found #{left_join_results.length} rows."
 
-# postgres@eoin=#  select * from users as u left join organisations as o on u.org_id = o.id;
+# postgres@eoin=#  SELECT * FROM users AS u LEFT JOIN organisations AS o ON u.org_id = o.id;
 #  id │ name  │ org_id │ id │  name
 # ════╪═══════╪════════╪════╪═════════
 #   1 │ joe   │      1 │  1 │ Foo Inc
@@ -226,7 +324,6 @@ puts "Found #{left_join_results.length} rows."
 # Found 4 rows.
 
 # ########################################################################### #
-# select * from users as u right join organisations as o on u.org_id = o.id;
 puts "*" * 80
 puts "users RIGHT JOIN organisations"
 puts ""
@@ -238,7 +335,7 @@ end
 pp right_join_results
 puts "Found #{right_join_results.length} rows."
 
-# postgres@eoin=#  select * from users as u right join organisations as o on u.org_id = o.id;
+# postgres@eoin=#  SELECT * FROM users AS u RIGHT JOIN organisations AS o ON u.org_id = o.id;
 #  id │ name  │ org_id │ id │  name
 # ════╪═══════╪════════╪════╪═════════
 #   1 │ joe   │      1 │  1 │ Foo Inc
@@ -260,9 +357,7 @@ puts "Found #{right_join_results.length} rows."
 #  [nil, nil, nil, 2, "Bar Inc."]]
 # Found 4 rows.
 
-
 # ########################################################################### #
-# select * from organisations as o left join users as u on u.org_id = o.id;
 puts "*" * 80
 puts "organisations LEFT JOIN users"
 puts ""
@@ -274,7 +369,7 @@ end
 pp left_join_results
 puts "Found #{left_join_results.length} rows."
 
-# postgres@eoin=#  select * from organisations as o left join users as u on u.org_id = o.id;
+# postgres@eoin=#  SELECT * FROM organisations AS o LEFT JOIN users AS u ON u.org_id = o.id;
 #  id │  name   │ id │ name  │ org_id
 # ════╪═════════╪════╪═══════╪════════
 #   1 │ Foo Inc │  1 │ joe   │      1
@@ -296,9 +391,7 @@ puts "Found #{left_join_results.length} rows."
 #  [2, "Bar Inc.", nil, nil, nil]]
 # Found 4 rows.
 
-
 # ########################################################################### #
-# select * from users as u full join organisations as o on u.org_id = o.id;
 puts "*" * 80
 puts "users FULL JOIN organisations"
 puts ""
@@ -310,7 +403,7 @@ end
 pp full_join_results
 puts "Found #{full_join_results.length} rows."
 
-# postgres@eoin=#  select * from users as u full join organisations as o on u.org_id = o.id;
+# postgres@eoin=#  SELECT * FROM users AS u FULL JOIN organisations AS o ON u.org_id = o.id;
 #  id │ name  │ org_id │ id │  name
 # ════╪═══════╪════════╪════╪═════════
 #   1 │ joe   │      1 │  1 │ Foo Inc
--- databases/postgresql/code/pg_play.rb
+++ databases/postgresql/code/pg_play.rb
@@ -1,11 +1,11 @@
-require 'pg'
+require "pg"
 
 # Output a table of current connections to the DB
-conn = PG.connect(dbname: 'eoin_play')
+conn = PG.connect(dbname: "eoin_play")
 conn.exec("SELECT * FROM pg_stat_activity") do |result|
   puts "     PID | User             | Query"
   result.each do |row|
     # puts row
-    puts " %7d | %-16s | %s " % row.values_at('pid', 'usename', 'application_name')
+    puts " %7d | %-16s | %s " % row.values_at("pid", "usename", "application_name")
   end
 end
